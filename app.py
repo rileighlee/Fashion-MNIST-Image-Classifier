8be81af43d3fb6eaaaa98bb857993aa6a4253d09
rileighlee <133644281+rileighlee@users.noreply.github.com>
2023-05-16 22:25:03 +0800

    app.py

--- app.py
+++ app.py
@@ -1,31 +1,33 @@
 import streamlit as st
 import tensorflow as tf
-import os
-import numpy as np
-from PIL import Image
 
-MODEL_DIR = "saved_models"  # Define the directory to save the model
-
-# Define class names for Fashion MNIST
-class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat', 
-               'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
-
-@st.cache(allow_output_mutation=True, hash_funcs={tf.keras.Model: id})
+@st.cache(allow_output_mutation=True)
 def load_model():
-    model_path = os.path.join(MODEL_DIR, "/content/drive/MyDrive/project/model.h5")
-    model = tf.keras.models.load_model(model_path)
-    return model
-
-st.write("# Clothes Detection System")
-file = st.file_uploader("Choose clothes photo from computer", type=["jpg", "png"])
-
+  model=tf.keras.models.load_model(/content/drive/MyDrive/project/model.h5')
+  return model
+model=load_model()
+st.write("""
+# Clothes Detection System"""
+)
+file=st.file_uploader("Choose clothes photo from computer",type=["jpg","png"])
+A
+import cv2
+from PIL import Image,ImageOps
+import numpy as np
+def import_and_predict(image_data,model):
+    size=(64,64)
+    image=ImageOps.fit(image_data,size,Image.ANTIALIAS)
+    img=np.asarray(image)
+    img_reshape=img[np.newaxis,...]
+    prediction=model.predict(img_reshape)
+    return prediction
 if file is None:
     st.text("Please upload an image file")
 else:
-    image = Image.open(file)
-    st.image(image, use_column_width=True)
-    model = load_model()  # Load the trained model
-    prediction = model.predict(np.array([np.array(image.resize((28, 28)))/255.0]))
-    predicted_class = class_names[np.argmax(prediction)]
-    string = "OUTPUT : " + predicted_class
+    image=Image.open(file)
+    st.image(image,use_column_width=True)
+    prediction=import_and_predict(image,model)
+    class_names=['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat', 
+               'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
+    string="OUTPUT : "+class_names[np.argmax(prediction)]
     st.success(string)
