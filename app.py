b9765862ea5a941f96ccf313363ec1435c87fd41
rileighlee <133644281+rileighlee@users.noreply.github.com>
2023-05-17 10:47:57 +0800

    app.py

--- app.py
+++ app.py
@@ -1,38 +1,34 @@
 import streamlit as st
 import tensorflow as tf
-import os
-
-MODEL_DIR = "saved_models"  # Define the directory to save the model
-
-@st.cache(allow_output_mutation=True, hash_funcs={tf.keras.Model: id})
-def load_model():
-  
-    model_path = os.path.join(MODEL_DIR, "model.h5")
-    model = tf.keras.models.load_model(model_path)
-    return model
-
-st.write("# Clothes Detection System")
-file = st.file_uploader("Choose clothes photo from computer", type=["jpg", "png"])
-
-import cv2
-from PIL import Image,ImageOps
+from tensorflow import keras
+from PIL import Image
 import numpy as np
-def import_and_predict(image_data,model):
-    size=(64,64)
-    image=ImageOps.fit(image_data,size,Image.ANTIALIAS)
-    img=np.asarray(image)
-    img_reshape=img[np.newaxis,...]
-    prediction=model.predict(img_reshape)
-    return prediction
-if file is None:
-    st.text("Please upload an image file")
-else:
-    image = Image.open(file)
-    st.image(image, use_column_width=True)
-    model = load_model()  # Load the trained model
-    prediction = model.predict(np.array([np.array(image.resize((28, 28)))/255.0]))
-    predicted_class = class_names[np.argmax(prediction)]
-    class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat', 
+
+model = keras.models.load_model('app.h5')
+
+class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
                'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
-    string = "OUTPUT : " + predicted_class
-    st.success(string)
+
+def preprocess_image(image):
+    image = image.resize((28, 28))
+    image = np.array(image)
+    image = image / 255.0
+    image = np.expand_dims(image, axis=-1)
+    image = np.expand_dims(image, axis=0)
+    return image
+  
+def app():
+    st.title('Fashion MNIST Image Classifier')
+    file = st.file_uploader('Upload a Fashion MNIST image', type=['jpg', 'png'])
+    
+    if file is not None:
+        image = Image.open(file)
+        st.image(image, caption='Uploaded Image', use_column_width=True)
+        image = preprocess_image(image)
+        prediction = model.predict(image)
+        class_index = np.argmax(prediction)
+        class_label = class_names[class_index]
+        st.write('The image is ', class_label)
+        
+if __name__ == '__main__':
+    app()
